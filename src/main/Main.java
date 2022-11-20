8dbc839859019e3769676c921971ac06218fda4d
yin <y.laiwang@uandresbello.edu>
2022-11-19 23:27:41 -0300

    primer comit

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author Yin
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("heloow");
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+}

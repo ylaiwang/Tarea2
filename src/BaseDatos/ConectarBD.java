5ca89477de8f348e70e800943901749ddb49f638
Dalark <v_e_c_g@outlook.com>
2022-11-21 00:28:58 -0300

    Creo la base para conectarse y cerrar la base de datos

--- src/BaseDatos/ConectarBD.java
+++ src/BaseDatos/ConectarBD.java
@@ -1,16 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package BaseDatos;
 
-/**
- *
- * @author Yin
- */
+//import javax.servlet.http.HttpServlet;
+
+import java.sql.Connection;
+import java.util.logging.Level;
+
+//import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
 public class ConectarBD {
     
+    Connection conexion;
+    private static final String user = "root";
+    private static final String password = "";
+    private static final String url = "jdbc:mysql://localhost:3306/porDefinir";
     
+    public Connection getConnection(){
+        // cualquier cosa
+        return null;
+    }
+    
+    
+    
+    public void cerrar(){
+        try{
+            conexion.close();
+        } catch(SQLException excepcion) {
+            Logger.getLogger(ConectarBD.class.getName()).log(Level.SEVERE, null, excepcion);
+        }
+    }
     
     
 }

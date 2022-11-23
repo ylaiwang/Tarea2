8cd10cab6ae7bdb0753966976cd1c626fb721e0f
Dalark <v_e_c_g@outlook.com>
2022-11-23 00:04:51 -0300

    creacion de la clase envia y modificar datos

--- src/BaseDatos/ModificarDatos.java
+++ src/BaseDatos/ModificarDatos.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package BaseDatos;
 
-/**
- *
- * @author v_e_c
- */
+
 public class ModificarDatos {
+    private final Conexion conexion;
     
+    public ModificarDatos()
+    {
+        conexion = new Conexion();
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,6 @@ import BaseDatos.Conexion;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Probar si se conecta");
-        
         Conexion date = new Conexion();
         date.getConnection();
     
